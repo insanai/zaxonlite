a67baa841617d7c77ec6adfe5b395e16c0aeae55
Vikrant Rathore <admin@vyomtech.com>
2026-08-27 11:33:08 +0800

    chore(zaxonlite): bump library, Python SDK, and C ABI version to 0.6.0

--- test/capi_smoke.c
+++ test/capi_smoke.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv) {
     snprintf(dir, sizeof dir, "%s-%d-%ld", argv[1], (int)getpid(),
              (long)time(NULL));
 
-    CHECK("version string", strcmp(zaxonlite_version(), "0.4.0") == 0);
+    CHECK("version string", strcmp(zaxonlite_version(), "0.6.0") == 0);
 
     zaxonlite *db = NULL;
     CHECK("open", zaxonlite_open(dir, &db) == 0 && db != NULL);
